dd4d896989fe719f08a9d4b4b8bf8f2aa785ff16
rena <66694960+rlilac@users.noreply.github.com>
2026-08-25 12:08:48 -0400

    dart tutorial #5 complete

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -4,7 +4,10 @@
 library;
 
 //makes the declarations in here available to other packages that import this pkg
+export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
+export 'src/help_command.dart';
+
 
 
 // TODO: Export any libraries intended for clients of this package.
